a3c4a2fd58240c0785930767822bf179d999f397
Nataliia <nata.lv.kr@gmail.com>
2020-07-24 14:14:03 +0200

    - fix issue seg-215 changing the input mask library

--- modules/components/widgets/antd/value/Text.jsx
+++ modules/components/widgets/antd/value/Text.jsx
@@ -1,7 +1,7 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import { Input, Col } from 'antd';
-import ReactInputMask from 'react-input-mask';
+import MaskedInput from 'react-text-mask';
 
 export default class TextWidget extends PureComponent {
   static propTypes = {
@@ -17,8 +17,18 @@ export default class TextWidget extends PureComponent {
   handleChange = (ev) => {
     const v = ev.target.value;
     const val = v === '' ? undefined : v; // don't allow empty value
+    if (val && (val === '.' || val.endsWith('..'))) return;
+    if (val) {
+      const parts = val.split('.');
+      if (
+          parts.length > 4 ||
+          parts.some(part => part === '00' || part < 0 || part > 255)
+      ) {
+        return;
+      }
+    }
     this.props.setValue(val, false, true);
-  }
+  };
 
   validateOnBlur() {
     if (!this.value) {
@@ -34,18 +44,38 @@ export default class TextWidget extends PureComponent {
 
     return (
         <Col>
-        <ReactInputMask
-            mask={mask}
-            onChange={this.handleChange} value={_value} onBlur={this.validateOnBlur.bind(this.props)} disabled={readonly}>
-          <Input
-          key="widget-text"
-          size={renderSize}
-          type={"text"}
-              value={_value}
-          placeholder={placeholder}
-          {...customProps}
-        />
-        </ReactInputMask>
+          {mask ?
+              (
+                  <MaskedInput
+                      mask={[/\d./]}
+                      guide={false}
+                      value={_value}
+                      showMask
+                      render={(ref, props) => {
+                        return (
+                            <Input
+                                name={name}
+                                value={_value}
+                                ref={(input) => ref(input && input.input)}
+                                {...props}
+                                onChange={this.handleChange}
+                                key="widget-text"
+                                size={renderSize}
+                                placeholder={placeholder}
+                                onBlur={this.validateOnBlur.bind(this.props)}
+                            />)}} />
+              ) : (
+            <Input
+                key="widget-text"
+                size={renderSize}
+                type={"text"}
+                value={_value}
+                placeholder={placeholder}
+                {...customProps}
+                onChange={this.handleChange}
+                onBlur={this.validateOnBlur.bind(this.props)}
+                disabled={readonly}
+            />)}
       </Col>)
   }
 }
