ca00112bfeb75cd3fd73db0f6fee4c39f9f3fe28
Nataliia <nata.lv.kr@gmail.com>
2020-08-04 14:12:50 +0200

    - new json parser fix

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -318,7 +318,7 @@ interface BaseOperator {
   sqlOp?: String,
   sqlFormatOp?: SqlFormatOperator,
   jsonLogic?: String | JsonLogicFormatOperator,
-  jsonLogic1?: String | JsonLogicFormatOperator,
+  jsonLogic2?: String | JsonLogicFormatOperator,
   _jsonLogicIsRevArgs?: Boolean,
   valueSources?: Array<ValueSource>,
 };
